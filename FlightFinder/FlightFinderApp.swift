e38511a0bcaf3101ebef0e412564a4132854595c
Samuel Silva <samuelh.dev@gmail.com>
2025-03-13 16:30:22 -0300

    Initial Commit

---
+++ FlightFinder/FlightFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightFinderApp.swift
+//  FlightFinder
+//
+//  Created by Samuel Silva on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FlightFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
