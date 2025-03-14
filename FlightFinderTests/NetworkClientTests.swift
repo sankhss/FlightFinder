2eaa4ed155b8d08c6591c44593f45b77b99b55d9
Samuel Silva <samuelh.dev@gmail.com>
2025-03-14 19:25:59 -0300

    test: assert that network client returns data on status 200

--- FlightFinderTests/NetworkClientTests.swift
+++ FlightFinderTests/NetworkClientTests.swift
@@ -8,28 +8,73 @@
 import XCTest
 
 final class NetworkClientTests: XCTestCase {
+    
+    func test_get_returnsData_whenStatusCode200() async throws {
+        let sut = NetworkClient()
+        let url = URL(string: "https://example.com")!
+        let expectedData = "Success".data(using: .utf8)
+        
+        URLProtocolStub.testResponse = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+        URLProtocolStub.testData = expectedData
+        
+        URLProtocolStub.startInterceptingRequests()
+        let data = try await sut.get(url: url)
+        URLProtocolStub.stopInterceptingRequests()
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        XCTAssertEqual(data, expectedData)
     }
+    
+    final class URLProtocolStub: URLProtocol {
+        static var testResponse: URLResponse?
+        static var testData: Data?
+        
+        override class func canInit(with request: URLRequest) -> Bool {
+            true
+        }
+        
+        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+            request
+        }
+        
+        override func startLoading() {
+            if let data = URLProtocolStub.testData {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            if let response = URLProtocolStub.testResponse {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
+            
+            client?.urlProtocolDidFinishLoading(self)
+        }
+        
+        override func stopLoading() { }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        static func startInterceptingRequests() {
+            URLProtocol.registerClass(URLProtocolStub.self)
+        }
+        
+        static func stopInterceptingRequests() {
+            URLProtocol.unregisterClass(URLProtocolStub.self)
+            testResponse = nil
+            testData = nil
         }
     }
+}
 
+protocol NetworkClientProtocol {
+    func get(url: URL) async throws -> Data
+}
+
+final class NetworkClient: NetworkClientProtocol {
+    private let session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func get(url: URL) async throws -> Data {
+        let (data, _) = try await session.data(from: url)
+        return data
+    }
 }
