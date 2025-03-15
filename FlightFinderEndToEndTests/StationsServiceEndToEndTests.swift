946fabda8638921a9ba264dd3ec1f6e46ff33b8e
Samuel Silva <samuelh.dev@gmail.com>
2025-03-15 16:08:27 -0300

    test: assert that real api returns data

--- FlightFinderEndToEndTests/StationsServiceEndToEndTests.swift
+++ FlightFinderEndToEndTests/StationsServiceEndToEndTests.swift
@@ -6,30 +6,17 @@
 //
 
 import XCTest
+@testable import FlightFinder
 
-final class FlightFinderEndToEndTests: XCTestCase {
+final class StationsServiceEndToEndTests: XCTestCase {
+    
+    func test_endToEndServerGETStationsResult() async throws {
+        let url = URL(string: "https://mobile-testassets-dev.s3.eu-west-1.amazonaws.com/stations.json")!
+        let client = NetworkClient()
+        let service = StationsService(networkClient: client, url: url)
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let stations = try await service.loadStations()
+        
+        XCTAssertFalse(stations.isEmpty, "Expected non-empty stations array")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
