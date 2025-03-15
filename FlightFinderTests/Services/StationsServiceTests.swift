44207837bc4087800b583222b745dd92b08c25d5
Samuel Silva <samuelh.dev@gmail.com>
2025-03-14 22:34:09 -0300

    test: assert that station service loads successfully on valid json

--- FlightFinderTests/Services/StationsServiceTests.swift
+++ FlightFinderTests/Services/StationsServiceTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 final class StationsServiceTests: XCTestCase {
     
-    func test_loadStations_doesNotreturnsStations_whenInvalidJSON() async throws {
+    func test_loadStations_doesNotReturnsStations_whenInvalidJSON() async throws {
         let jsonData = try JSONEncoder().encode("invalid".data(using: .utf8))
         
         let clientSpy = NetworkClientSpy()
@@ -25,6 +25,36 @@ final class StationsServiceTests: XCTestCase {
         } catch {
             XCTAssertTrue(true)
         }
+    }
+    
+    func test_loadStations_returnsStations_validJSON() async throws {
+        let station1 = makeStation(code: "AAR", name: "Aarhus")
+        let station2 = makeStation(code: "CPH", name: "Copenhagen")
+        let expectedStations = [station1.model, station2.model]
+        let jsonData = makeStationsJSON([station1.json, station2.json])
+        
+        let clientSpy = NetworkClientSpy()
+        clientSpy.stubbedData = jsonData
+        
+        let url = URL(string: "https://mobile-testassets-dev.s3.eu-west-1.amazonaws.com/stations.json")!
+        let sut = StationsService(networkClient: clientSpy, url: url)
+        
+        let stations = try await sut.loadStations()
+        XCTAssertEqual(stations, expectedStations)
+    }
+    
+    private func makeStation(code: String, name: String) -> (model: Station, json: [String: Any]) {
+        let model = Station(code: code, name: name)
+        let json: [String: Any] = [
+            "code": code,
+            "name": name,
+        ]
+        return (model, json)
+    }
+
+    private func makeStationsJSON(_ items: [[String: Any]]) -> Data {
+        let json = ["stations": items]
+        return try! JSONSerialization.data(withJSONObject: json, options: [])
     }
     
     final class NetworkClientSpy: NetworkClientProtocol {
