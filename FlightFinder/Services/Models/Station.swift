755de5446f2190aceb390beccffae3f42c0717a9
Samuel Silva <samuelh.dev@gmail.com>
2025-03-14 22:18:17 -0300

    refactor: add fields by demand

--- FlightFinder/Services/Models/Station.swift
+++ FlightFinder/Services/Models/Station.swift
@@ -9,9 +9,5 @@ import Foundation
 
 public struct Station: Codable, Equatable {
     public let code: String
-    public let countryCode: String
     public let name: String
-    public let latitude: String
-    public let longitude: String
-    public let mobileBoardingPass: Bool
 }
