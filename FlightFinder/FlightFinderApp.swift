7f84d9391f115d488a3dbfd81f23e2efdb4c254a
Samuel Silva <samuelh.dev@gmail.com>
2025-03-18 02:48:36 -0300

    feat: inject dependencies on app launch

--- FlightFinder/FlightFinderApp.swift
+++ FlightFinder/FlightFinderApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct FlightFinderApp: App {
+    
+    private let flightService = FlightService(client: NetworkClient(), url: APIConstants.Endpoints.flightAvailability)
+    private let stationsService = StationsService(client: NetworkClient(), url: APIConstants.Endpoints.stations)
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FlightSearchView(viewModel: FlightSearchViewModel(stationsService: stationsService, flightService: flightService))
         }
     }
 }
